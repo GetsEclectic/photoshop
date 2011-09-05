22f485a637a0ebe08655afd30b1dd2054738e314
U-hmsbounty\NotALamer <NotALamer@hmsbounty.(none)>
2011-09-04 21:01:00 -0400

    added spacing offset to top left

--- src/gradients.js
+++ src/gradients.js
@@ -10,7 +10,7 @@ var ad = activeDocument;
 var numColors = 32;
 var swatchWidth = 64;
 var swatchHeight = 64;
-var spacing = 1.2;
+var spacing = 0.2;
 
 var gradientColor = new SolidColor;
 gradientColor.hsb.brightness = 0;
@@ -23,15 +23,15 @@ function getBWGradientColor(colorIndex, numColors){
     return gradientColor;
 }
 
-var numSquaresPerRow = Math.floor(ad.width.value / (spacing * swatchWidth));
+var numSquaresPerRow = Math.floor(ad.width.value / ((1+spacing) * swatchWidth));
 var rowIndex = 0;
 var columnIndex = 0;
 
 for(var i = 0; i <= numColors; i++){
     var newColor = getBWGradientColor(i, numColors);
 
-    var xCoord = swatchWidth * spacing * columnIndex;
-    var yCoord = swatchHeight * spacing * rowIndex;
+    var xCoord = swatchWidth * (1+spacing) * columnIndex + spacing * swatchWidth;
+    var yCoord = swatchHeight * (1+spacing) * rowIndex + spacing * swatchHeight;
 
     ad.selection.select([[xCoord,yCoord],[xCoord+swatchWidth,yCoord],[xCoord+swatchWidth,yCoord+swatchHeight],[xCoord,yCoord+swatchHeight]],
         SelectionType.REPLACE, 0, false);
